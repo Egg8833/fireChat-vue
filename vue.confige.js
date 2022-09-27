cc0deb63faf2844afde6ee55976e332b27469c05
黃禎平 <egg8833@gmail.com>
2022-09-27 11:50:26 +0800

    'add'

--- vue.confige.js
+++ vue.confige.js
@@ -1,3 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+
+  
+})
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/fireChat-vue/' //  為 repo 名稱
